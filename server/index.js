452ea7dcb5f766093a2bccdb794d682f4b5ee26d
Nikola Ignatov <nik.ignatovv@gmail.com>
2018-11-29 12:06:22 +0000

    added express

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+
 var ttn = require("ttn");
 
 var appID = "kentwatersensors"
@@ -6,18 +9,25 @@ var accessKey = "ttn-account-v2.7j6Z9OduNwFW7il2Sd28YYF4Q-8l9rDDPaNRFw06-GM"
 var hexPayload;
 var distance;
 
-ttn.data(appID, accessKey)
-  .then(function (client) {
-    client.on("uplink", function (devID, payload) {
-      console.log("Received uplink from ", devID)
-      console.log(payload)
-      hexPayload = Buffer.from(payload.payload_raw, 'base64').toString('hex');
-      distance = parseInt(hexPayload, 16);
-      console.log(hexPayload);
-      console.log(distance);
+app.listen(5000, () => {
+  console.log('listening on 5000');
+})
+
+app.get('/test', (req, res) => {
+  ttn.data(appID, accessKey)
+    .then(function (client) {
+      client.on("uplink", function (devID, payload) {
+        console.log("Received uplink from ", devID)
+        console.log(payload)
+        hexPayload = Buffer.from(payload.payload_raw, 'base64').toString('hex');
+        distance = parseInt(hexPayload, 16);
+        console.log(hexPayload);
+        console.log(distance);
+      })
     })
-  })
-  .catch(function (error) {
-    console.error("Error", error)
-    process.exit(1)
-  })
+    .catch(function (error) {
+      console.error("Error", error)
+      process.exit(1)
+    })
+  res.send(distance)
+})
