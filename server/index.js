e2689cac4b0cf02971e42f2858e9d43ab63b1097
Ivan Kasabov <iv.kasabov@gmail.com>
2018-12-03 19:58:03 +0000

    Added query to read the db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
+const request = require('request');
 const express = require('express');
 const app = express();
+//dsrb = distance_sensor_from_river_bed
+//dfpfrb distance_flood_plain_from_river_bed
+const dsrb_f3 = 1820;
+const dfpfrb_f3 = 1820;
+const dsrb_45 = 1340;
+const dfpfrb_45 = 1200;
 
 var ttn = require("ttn");
 var mysql = require("mysql");
@@ -12,6 +19,15 @@ var accessKey = options.storageConfig.accessKey;
 
 var hexPayload; //distance to water (hex)
 var distance; //distance to water in mm
+var floodAlert = false;
+
+//the 'request' package supports HTTPS and follows redirects by default :-)
+request
+  .get('https://environment.data.gov.uk/flood-monitoring/id/stations/E3826/measures?parameter=level')
+  // .get('https://environment.data.gov.uk/flood-monitoring/id/stations/E3826/readings?latestReading_limit=1')
+  .on('response', function(response) {
+    // console.log(JSON.stringify(response))
+  })
 
 var con = mysql.createConnection({
   host: options.storageConfig.host,
@@ -51,3 +67,14 @@ ttn.data(appID, accessKey)
     console.error("Error: ", error)
     process.exit(1)
   })
+
+  function getDataForPeriod(dateFrom,dateTo){
+    params = [dateFrom,dateTo];
+    sql = "SELECT timestamp,distanceToSensor FROM ni60.log WHERE CAST(timestamp AS DATE) BETWEEN ? AND ?";
+    con.query(sql, params, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+
+  getDataForPeriod('2018-12-02','2018-12-03');
