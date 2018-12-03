c9d95d41dd98dcd4c3853f28323e08298cabe056
Ivan Kasabov <iv.kasabov@gmail.com>
2018-12-03 22:30:12 +0000

    Restructured database manipulation code

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
 const request = require('request');
 const express = require('express');
 const app = express();
-//dsrb = distance_sensor_from_river_bed
-//dfpfrb distance_flood_plain_from_river_bed
-const dsrb_f3 = 1820;
-const dfpfrb_f3 = 1820;
-const dsrb_45 = 1340;
-const dfpfrb_45 = 1200;
+const sensor_f3 = "lairdc0ee4000010109f3"; //The sensor with id 'lairdc0ee4000010109f3'
+const distance_sensor_from_river_bed_sensor_f3 = 1820;
+const distance_flood_plain_from_river_bed_sensor_f3 = 1820;
+const sensor_45 = "lairdc0ee400001010945"; //The sensor with id 'lairdc0ee400001010945'
+const distance_sensor_from_river_bed_sensor_45 = 1340;
+const distance_flood_plain_from_river_bed_sensor_45 = 1200;
 
 var ttn = require("ttn");
-var mysql = require("mysql");
+var queryHandler = require('./queryHandler');
 
-//Get credentials
-var options = require('./options');
-
+var options = require('./options'); //The parsed options file
 var appID = options.storageConfig.appID;
 var accessKey = options.storageConfig.accessKey;
 
@@ -21,6 +19,7 @@ var hexPayload; //distance to water (hex)
 var distance; //distance to water in mm
 var floodAlert = false;
 
+//TODO figure out what to pull from gov data below
 //the 'request' package supports HTTPS and follows redirects by default :-)
 request
   .get('https://environment.data.gov.uk/flood-monitoring/id/stations/E3826/measures?parameter=level')
@@ -29,52 +28,45 @@ request
     // console.log(JSON.stringify(response))
   })
 
-var con = mysql.createConnection({
-  host: options.storageConfig.host,
-  user: options.storageConfig.user,
-  password: options.storageConfig.pwd
-});
-
-//Make sure there is a connection to the database
-con.connect(function(err) {
-  if (err) throw err;
-});
-
 //receive data and add it to a database
 ttn.data(appID, accessKey)
   .then(function(client) {
     client.on("uplink", function(devID, payload) {
-
       console.log("Received uplink from: " + devID);
       console.log(payload);
       hexPayload = Buffer.from(payload.payload_raw, 'base64').toString('hex'); //the distance in hex format
       distance = parseInt(hexPayload, 16); //the integer value (distance in mm)
 
-      var queryData_Log = {
+      var distance_sensor_from_river_bed;
+      var distance_flood_plain_from_river_bed;
+
+      switch (devID) {
+        case sensor_45:
+          distance_sensor_from_river_bed = distance_sensor_from_river_bed_sensor_45;
+          distance_flood_plain_from_river_bed = distance_flood_plain_from_river_bed_sensor_45;
+          break;
+        case sensor_f3:
+          distance_sensor_from_river_bed = distance_sensor_from_river_bed_sensor_f3;
+          distance_flood_plain_from_river_bed = distance_flood_plain_from_river_bed_sensor_f3;
+          break;
+      }
+
+      //TODO handle the 300mm difference
+      if (distance <= distance_sensor_from_river_bed - distance_flood_plain_from_river_bed) {
+        console.log('SHIIT FLOOD GET THE BOAT');
+      } else {
+        console.log('NO flood');
+      }
+
+      var params = {
         timestamp: payload.timestamp,
-        dev_id: payload.dev_id,
+        dev_id: devID,
         distanceToSensor: distance
       };
-
-      sql = "INSERT INTO ni60.log SET ?";
-      con.query(sql, queryData_Log, function(err, result) {
-        if (err) throw err;
-        console.log("1 record added to the log");
-      });
+      queryHandler.insertLogRecord(params);
     })
   })
   .catch(function(error) {
     console.error("Error: ", error)
     process.exit(1)
   })
-
-  function getDataForPeriod(dateFrom,dateTo){
-    params = [dateFrom,dateTo];
-    sql = "SELECT timestamp,distanceToSensor FROM ni60.log WHERE CAST(timestamp AS DATE) BETWEEN ? AND ?";
-    con.query(sql, params, function(err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
-  }
-
-  getDataForPeriod('2018-12-02','2018-12-03');
