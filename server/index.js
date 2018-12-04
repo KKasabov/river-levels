02312a1bff1d9c798f57fdd54e51d220fc8db983
Ivan Kasabov <iv.kasabov@gmail.com>
2018-12-04 15:32:35 +0000

    Updated query for graph data

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const request = require('request');
 const express = require('express');
 const app = express();
+const API_PORT = 8080;
+const router = express.Router();
 const sensor_f3 = "lairdc0ee4000010109f3"; //The sensor with id 'lairdc0ee4000010109f3'
 const distance_sensor_from_river_bed_sensor_f3 = 1820;
 const distance_flood_plain_from_river_bed_sensor_f3 = 1820;
-const sensor_45 = "lairdc0ee400001010945"; //The sensor with id 'lairdc0ee400001010945'
+const sensor_45 = "lairdc0ee400001012345"; //The sensor with id 'lairdc0ee400001012345'
 const distance_sensor_from_river_bed_sensor_45 = 1340;
 const distance_flood_plain_from_river_bed_sensor_45 = 1200;
-
-const API_PORT = 8080;
-const app = express();
-const router = express.Router();
 
 var ttn = require("ttn");
 var queryHandler = require('./queryHandler');
@@ -25,13 +23,14 @@ var floodAlert = false;
 
 //TODO figure out what to pull from gov data below
 //the 'request' package supports HTTPS and follows redirects by default :-)
-request
-  .get('https://environment.data.gov.uk/flood-monitoring/id/stations/E3826/measures?parameter=level')
-  // .get('https://environment.data.gov.uk/flood-monitoring/id/stations/E3826/readings?latestReading_limit=1')
-  .on('response', function(response) {
-    // console.log(JSON.stringify(response))
-  })
-
+function getNearestGovSensor() {
+  request
+    .get('https://environment.data.gov.uk/flood-monitoring/id/stations/?lat=51.280233&long=1.0789089&dist=5')
+    .on('response', function(response) {
+      // console.log(response)
+    })
+}
+getNearestGovSensor();
 //receive data and add it to a database
 ttn.data(appID, accessKey)
   .then(function(client) {
@@ -81,8 +80,8 @@ ttn.data(appID, accessKey)
 // this is our get method
 // this method fetches all available data in our database
 router.get("/getData", (req, res) => {
-  queryHandler.getDataForPeriod('2018-12-03', '2018-12-03').then(function(rows) {
-    res.json(result);
+  queryHandler.getDataForPeriod(sensor_45,'2018-12-03', '2018-12-03').then(function(rows) {
+    res.json(rows);
   }).catch((err) => setImmediate(() => {
     throw err;
   }));
