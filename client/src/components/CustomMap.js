a9a4ab208d2a2dcf7a48233a44b4bf5263cd5462
Daniel Krastev <dkk6@kent.ac.uk>
2018-12-05 20:56:18 +0000

    Marker move detection.

--- client/src/components/CustomMap.js
+++ client/src/components/CustomMap.js
@@ -1,53 +1,112 @@
-import React, { createRef, Component } from 'react';
-import { LayerGroup, LayersControl, ZoomControl, Map, TileLayer, Marker, Popup, withLeaflet } from 'react-leaflet';
+import React, { Component } from 'react';
+import { LayerGroup, LayersControl, ZoomControl, Map, TileLayer, Marker, Popup, withLeaflet, Tooltip, Polygon } from 'react-leaflet';
 import './CustomMap.css';
 import AddressControl from './AddressControl'
 import sensorMarker from '../resources/sensorMarker.png';
+import sensorMarkerCBlind from '../resources/sensorMarkerCBlind.png';
 import L from 'leaflet';
+import test from './test'
 
-var myIcon = L.icon({
-  iconUrl: sensorMarker,
-  iconSize: [41, 41],
-  iconAchor: [22, 94],
-  popupAnchor: [0, -10]
-});
+const sensorPositions = [
+  [51.257785, 1.030079],
+  [51.258144, 1.145929],
+  [51.296693, 1.105983],
+  [51.258867, 1.033447],
+  [51.297928, 1.053239],
+  [51.296461, 1.129525],
+  [51.278570, 1.0770049],
+  [51.280064, 1.0733199],
+];
+
+const multiPolygon = [
+  [[51.51, -0.12], [51.51, -0.13], [51.53, -0.13]],
+  [[51.51, -0.05], [51.51, -0.07], [51.53, -0.07]],
+]
+
+const polygon = [[51.515452323423452345, -0.093452323423423452345], [51.52123123423423423123, -0.15323423423445345], [51.52123123423823423123, -0.15323493423445345], [51.23423423423423423, -0.121231523423423421535]]
 
 const { BaseLayer, Overlay } = LayersControl
 
 class CustomMap extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       cityCenter: [51.2802, 1.0789],
-      zoom: 12
+      zoom: 12,
+      marker: {},
     }
+
+    this.myIcon = L.icon({
+      iconUrl: this.props.isColorBlind ? sensorMarkerCBlind : sensorMarker,
+      iconSize: [41, 41],
+      iconAchor: [22, 94],
+      popupAnchor: [0, -10]
+    });
   }
 
-
-
   componentDidMount() {
-    console.log(this.refs.map.leafletElement);
-    this.refs.map.leafletElement.on('move', e => {
-      console.log(e.target._animateToCenter);
+    let arr;
+    this.refs.map.leafletElement.on('moveend', e => {
+      arr = Object.values(this.refs.map.leafletElement._targets);
+      if(arr[arr.length - 1] != null && arr.length > sensorPositions.length ) {
+        this.setState({marker: arr[arr.length - 1]});
+        this.state.marker.on('dragend', e => {
+          this.state.marker._popup._content = "Home";
+          this.setState({marker: arr[arr.length - 1]});
+          this.state.marker._popup._content = "Home";
+        });
+      }
     });
   }
 
   componentDidUpdate(prevProps) {
-
+    if(prevProps.isColorBlind !== this.props.isColorBlind) {
+      //setIcons for sensors and search
+    }
   }
 
   componentWillUnmount() {
 
   }
 
-  render() {
-    // var position1 = [this.props.lat1, this.props.lng1];
-    // var position2 = [this.props.lat2, this.props.lng2];
-    // var position3 = [this.props.lat3, this.props.lng3];
-    var position1 = [51.296461, 1.129525];
-    var position2 = [51.27857, 1.0770049];
-    var position3 = [51.280064, 1.0733199];
+  createPolygon(polyObj) {
+    // var arr = polyObj.features[0].geometry.coordinates[0];
+    // var revArr = this.reverseArr(arr, 0);
+    // console.log(arr);
+    // console.log(revArr);
+    return (
+      <Polygon color="purple" positions={polyObj.features[0].geometry.coordinates} />
+    );
+  }
 
+  // reverseArr(coordinates, c) {
+  //   if(isNaN(coordinates[c])) {
+  //     return this.reverseArr(coordinates[c], 0);
+  //   } else if(!isNaN(coordinates[c])) {
+  //     var buff = coordinates[0];
+  //     coordinates[0] = coordinates[1];
+  //     coordinates[1] = buff;
+  //     return this.reverseArr(coordinates, c++);
+  //   } else {
+  //     return coordinates;
+  //   }
+  // }
+
+  createSensorMarkers(position, reading) {
+    return (
+      <Marker
+        position={position}
+        icon={this.myIcon}
+        >
+        <Tooltip>
+          {reading}
+        </Tooltip>
+      </Marker>
+    );
+  }
+
+  render() {
     const AddressSearch = withLeaflet(AddressControl);
     return (
       <Map
@@ -61,39 +120,32 @@ class CustomMap extends Component {
         <ZoomControl position="topleft" />
         <AddressSearch />
         <LayersControl position="topright">
-          <BaseLayer checked name="Colour">
+          <BaseLayer checked={this.props.isColorBlind} name="Colour">
             <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           </BaseLayer>
-          <BaseLayer name="Black And White">
+          <BaseLayer checked={this.props.isColorBlind} name="Black And White">
             <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png" />
           </BaseLayer>
+          <Overlay checked name="Active Alerts">
+            <LayerGroup>
+
+            </LayerGroup>
+          </Overlay>
+          <Overlay checked name="Canterbury Flood Areas">
+            <LayerGroup>
+              {this.createPolygon(test)}
+            </LayerGroup>
+          </Overlay>
           <Overlay checked name="Sensors">
             <LayerGroup>
-              <Marker
-                position={position1}
-                icon={myIcon}
-                >
-                <Popup>
-                  READING1
-                </Popup>
-              </Marker>
-              <Marker
-                position={position2}
-                icon={myIcon}
-                >
-                <Popup>
-                  {this.props.sensor_f3_reading}
-                </Popup>
-              </Marker>
-
-              <Marker
-                position={position3}
-                icon={myIcon}
-                >
-                <Popup>
-                  {this.props.sensor_45_reading}
-                </Popup>
-              </Marker>
+              {this.createSensorMarkers(sensorPositions[0], "Reading_EA_0")}
+              {this.createSensorMarkers(sensorPositions[1], "Reading_EA_1")}
+              {this.createSensorMarkers(sensorPositions[2], "Reading_EA_2")}
+              {this.createSensorMarkers(sensorPositions[3], "Reading_EA_3")}
+              {this.createSensorMarkers(sensorPositions[4], "Reading_EA_4")}
+              {this.createSensorMarkers(sensorPositions[5], "Reading_EA_5")}
+              {this.createSensorMarkers(sensorPositions[6], this.props.sensor_f3_reading)}
+              {this.createSensorMarkers(sensorPositions[7], this.props.sensor_45_reading)}
             </LayerGroup>
           </Overlay>
         </LayersControl>
